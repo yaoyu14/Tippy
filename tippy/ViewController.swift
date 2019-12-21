577698d13732fbe34d663b9eae2b5d766412d4a6
Yao Yu <yao97ming@gmail.com>
2019-12-21 14:14:44 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Yao Yu on 12/21/19.
+//  Copyright © 2019 Yao Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tippyTests/tippyTests.swift
@@ -0,0 +1,34 @@
+//
+//  tippyTests.swift
+//  tippyTests
+//
+//  Created by Yao Yu on 12/21/19.
+//  Copyright © 2019 Yao Yu. All rights reserved.
+//
+
+import XCTest
+@testable import tippy
+
+class tippyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
